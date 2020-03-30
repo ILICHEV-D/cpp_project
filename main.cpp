19a9dfc0522f52c19bae993df957a0b9040a2c8d
ILICHEV-D <58857267+ILICHEV-D@users.noreply.github.com>
2020-03-30 19:52:25 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
-//typedef vector<vector<int>> element_tablic;
-typedef vector<vector<tuple<int, string, string>>> element_tablic;
+typedef vector<vector<tuple<int, string, string>>> element_tablic;  // тип данных для быстрого обращеня
     
 extern int posledn_elem_c1 = 0;
+extern int kolich_vershin = 0;
 
 
-void VivodTabl(element_tablic& kok){
+void VivodTabl(element_tablic& kok){    // выводит таблицу для удобства пользователя
     int dl, sh;
     sh = 1;
     dl = 1;
     cout << "   ";
-    for (int dlina = 1; dlina <= 10; ++dlina){
-        cout << dlina << " ";
+    for (int dlina = 1; dlina <= kolich_vershin; ++dlina){
+        cout << setw(3) << dlina;
     }
     cout << endl;
     for (auto i: kok){
-        cout << sh << ": ";
+        cout << setw(2) << sh << ": ";
         ++sh;
         for (auto j: i){
-            cout << get<0>(j) << " ";
+            cout << setw(2) << get<0>(j) << " ";
             }
         cout << endl;
     }
 }
 
-
-void Nachalo(int ishodnoe_polozh, element_tablic& kok){
+ 
+void Nachalo(int ishodnoe_polozh, element_tablic& kok){      //построение первых вершин графы
     cout << "Сколько развилок вы хотите?" << endl;
     int kolichestvo_razvilok;
     cin >> kolichestvo_razvilok;
@@ -48,7 +49,7 @@ void Nachalo(int ishodnoe_polozh, element_tablic& kok){
     cout << endl;
 }
 
-void Prodol(int ishodnoe_polozh, element_tablic& kok){
+void Prodol(int ishodnoe_polozh, element_tablic& kok){    //построение вершин графа
     cout << "Сколько развилок вы хотите?" << endl;
     int kolichestvo_razvilok;
     cin >> kolichestvo_razvilok;
@@ -67,24 +68,28 @@ void Prodol(int ishodnoe_polozh, element_tablic& kok){
     posledn_elem_c1 += kol_raz2;
 }
  
-void Dobavit_text1(int otkuda, int kuda, element_tablic& kok) {
+void Dobavit_text1(int otkuda, int kuda, element_tablic& kok) {    //текст вершины, когда на нее ещё не зашли
     string new_text;
     getline(cin, new_text);
     cout << "Введите текст: ";
     cin >> new_text;
     string text_ishod = get<2>(kok[otkuda-1][kuda-1]);
     kok[otkuda-1][kuda-1] = make_tuple(1, new_text, text_ishod);
-    cout << endl;
     }
 
-void Dobavit_text2(int otkuda, int kuda, element_tablic& kok) {
-    string new_text;
-    getline(cin, new_text);
+void text_pri_popad_na_versh(element_tablic& kok){    //текст вершины, когда на нее уже зашли
+    cout << "Введите номер вершины из которой выходит герой (см. по Y): ";
+    int nomer;
+    cin >> nomer;
     cout << "Введите текст: ";
-    cin >> new_text;
-    string text_ishod = get<1>(kok[otkuda-1][kuda-1]);
-    kok[otkuda-1][kuda-1] = make_tuple(1, text_ishod, new_text);
-    cout << endl;
+    string text;
+    getline(cin, text);
+    cin >> text;
+    for (int i = 0; i != kolich_vershin; ++i){
+        int chislo = get<0>(kok[nomer-1][i]);
+        string text_old = get<1>(kok[nomer-1][i]);
+        kok[nomer-1][i] = make_tuple(chislo, text_old, text);
+    };
 }
 
 vector<int> DostupnieVershini(element_tablic kok_, int number){   //показывает списки вершин, в которые можно перейти из данной вершины
@@ -119,50 +124,58 @@ void infa_element(element_tablic kok){
     cout << "Текст исхода: " << get<2>(kok[y_-1][x_-1]) << endl;
 }
 
-void text_pri_popad_na_versh(element_tablic& kok){
-    cout << "Введите номер вершины из которой выходит герой (см. по Y): ";
-    int nomer;
-    cin >> nomer;
-    cout << endl;
-    cout << "Введите текст: ";
-    string text;
-    getline(cin, text);
-    cin >> text;
-    cout << endl;
-    for (int i = 0; i != 10; ++i){
-        int chislo = get<0>(kok[nomer-1][i]);
-        string text_old = get<1>(kok[nomer-1][i]);
-        kok[nomer-1][i] = make_tuple(chislo, text_old, text);
-        
-    //    cout << "Текст исхода: " << get<2>(kok[nomer-1][i]) << endl; // потом удалить
-    };
+void add_new_vershina(element_tablic& kok){
+    cout << "Введите значение элемента по Х:";
+    int x;
+    cin >> x;
+    cout << "Введите значение элемента по Y:";
+    int y;
+    cin >> y;
+    kok[y-1][x-1] = make_tuple(1,"", "");
 }
-
+    
+    
 void Game(const element_tablic& kok){
     int chetchik = 1;
     int place = 1;
+    bool check = true;
     while (chetchik != 0){
         cout << get<2>(kok[place-1][1]) << endl;
-        vector<int> nomera_vershin;
         int i = 0;
+        bool check = false;
         for (auto k: kok[place-1]){
             ++i;
             if (get<0>(k) == 1){
                 cout << i << " - " << get<1>(k) << endl;
+                check = true;
             }
          }
+        if (check == false){
+            chetchik = 0;
+            cout << "Конец игры" << endl;
+        }
+        else {
         cout << "Выберите номера вашего хода: ";
         cin >> place;
+        }
     }
+}
+
+void komandi(){
+        cout << "Выберите команду:" << endl << " 0 - 'Вывести список команд'" << endl << " 1 - 'Продолжить создавать разветвления'" << endl << " 2 - 'Добавить текст перехода из одной вершины в другую'" << endl << " 3 - 'Добавить текст при попадании на данную вершину'" << endl << " 4 - 'Завершить'" << endl << " 5 - 'Информация о вершине'" << endl << " 6 - 'Связать две вершины'" << endl << " 7 - 'Вывести таблицу'" << endl <<" 8 - 'Начать игру'" << endl;
 }
 
 int main()
 {
+    
+  //  int kolich_vershin;
+    cout << "Сколько будет вершин?" << endl;
+    cin >> kolich_vershin;
     element_tablic kok;
-    kok.resize(10);
-    for (int i = 0; i != 10; ++i) {
-        kok[i].resize(10);
-        for (int j = 0; j != 10; ++j) {
+    kok.resize(kolich_vershin);
+    for (int i = 0; i != kolich_vershin; ++i) {
+        kok[i].resize(kolich_vershin);
+        for (int j = 0; j != kolich_vershin; ++j) {
             kok[i][j] = make_tuple(0, " ", " ");
             }
     }
@@ -171,14 +184,14 @@ int main()
     VivodTabl(kok);
 
     
-    cout << "Введите 'продолжить' или 'завершить' или 'дов' = 'Текст перехода' или 'п' = Текст при попадании на данную вершину или 'игра'" << endl;
+    komandi();
 //    string komanda;
   //  getline(cin, komanda);
     int chetchik = 1;
     while (chetchik != 0){
         string komanda;
         getline(cin, komanda);
-        if (komanda == "продолжить") {
+        if (komanda == "1") {
             cout << "Введите номер начала развилки: ";
             int nomer;
             cin >> nomer;
@@ -189,32 +202,53 @@ int main()
             cout << "Введите команду: ";
         }
         
-        if (komanda == "завершить") {
+        if (komanda == "4") {
             chetchik = 0;
         }
         
-        if (komanda == "дов") {
-            cout << "Введите номер вершины из которой выходит герой: (см. по Y)";
+        if (komanda == "2") {
+            cout << "Введите номер вершины из которой выходит герой(см. по Y): ";
             int nomer1;
             cin >> nomer1;
-            cout << "Введите номер вершины в которую входит герой: (см. по X)";
+            cout << "Введите номер вершины в которую входит герой(см. по X): ";
             int nomer2;
             cin >> nomer2;
             Dobavit_text1(nomer1, nomer2, kok);
-            
-            string komanda;
-            getline(cin, komanda);
+            cout << "Готово" << endl;
+            cout << "Введите команду: ";
         }
         
-        if (komanda == "инфа") {
+        if (komanda == "5") {
             infa_element(kok);
+            cout << "Готово" << endl;
+            cout << "Введите команду: ";
         }
-        if (komanda == "п") {
+        if (komanda == "3") {
             text_pri_popad_na_versh(kok);
+            cout << "Готово" << endl;
+            cout << "Введите команду: ";
         }
         
-        if (komanda == "игра") {
+        if (komanda == "8") {
             Game(kok);
+            cout << "Готово" << endl;
+            cout << "Введите команду: ";
+        }
+        
+        if (komanda == "7") {
+            VivodTabl(kok);
+            cout << "Введите команду: ";
+        }
+        
+        if (komanda == "0"){
+            komandi();
+            cout << "Введите команду: ";
+        }
+        
+        if (komanda == "6"){
+        add_new_vershina(kok);
+        cout << "Готово" << endl;
+        cout << "Введите команду: ";
         }
     }
 
